746987aaa188a625f270233304d87798fc4f5949
Sahil Kathoke <49656406+boyknowstech@users.noreply.github.com>
2023-04-16 22:23:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add any JavaScript functionality you want here, such as animations or interactive elements.
+// For example, you can use the following code to display an alert when the page loads:
+
+window.onload = function() {
+  alert("Happy Birthday!");
+};
